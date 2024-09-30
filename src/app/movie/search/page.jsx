81563f95ac5beb03fc5ba66dd28e85803c560ca2
Navodi20 <navodidhananjana2002@gmail.com>
2024-09-30 10:58:31 +0530

    changes

--- src/app/movie/search/page.jsx
+++ src/app/movie/search/page.jsx
@@ -44,7 +44,24 @@ const MoviesTable = ({ data }) => (
     </tbody>
   </table>
 );
-
+const imageUrls = [
+  '/images/image 2 (1).jpg', 
+  '/images/image 2 (2).jpg',
+  '/images/image 2 (3).jpg',
+  '/images/image 2 (4).jpg',
+  '/images/image 2 (5).jpg',
+  '/images/image 2 (6).jpg',
+  '/images/image 2 (7).jpg',
+  '/images/image 2 (8).jpg',
+  '/images/image 2 (9).jpg',
+  '/images/image 2 (10).jpg',
+  '/images/image 2 (11).jpg',
+  '/images/image 2 (12).jpg',
+  '/images/image 2 (13).jpg',
+  '/images/image 2 (14).jpg',
+  '/images/image 2 (15).jpg',
+  '/images/image 2 (16).jpg'
+];
 const SearchPage = () => {
   const [searchTerm, setSearchTerm] = useState('');
   const { data } = useSWR(
@@ -60,8 +77,14 @@ const SearchPage = () => {
       <center><a href="/" className={styles.homeLink}>Back to Home</a></center>
 
       {data && <MoviesTable data={data} />}
-      
+      <div className="imageContainer">
+      {imageUrls.map((src, index) => (
+          <img key={index} src={src} className="image" />
+        ))}
+      </div>
     </div>
+    
+    
   );
 };
 
